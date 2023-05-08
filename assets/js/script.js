017c6b0e9aaa475a84506bc604a36751ed38ad23
LCatSew <119518206+LCatSew@users.noreply.github.com>
2023-05-07 20:55:08 -0500

    1. local storage is set to set and get your high score 2. timer stops when you run out of questions 3. seconds left is your score 4. if you run out of time the game ends and the timer stops. work to do: still need to be taken to a high score page that displays all previous high scores

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,33 @@
-//PSEUDO CODING//
-
-//Step one: timer//
-// ****NEEDS FURTHER DEV**** //
-// timer is working but it needs to stop once the quiz is finished
-// time still needs to go down by 10 seconds once the wrong answer is clicked
 var timeEl = document.querySelector("#time");
 var secondsLeft = 76; //so that it starts counting down from 75 seconds
 
 var startBtn = document.querySelector("#startBtn");
-
-startBtn.addEventListener("click", startTimer); //adds event listener to start button to start the timer
+startBtn.addEventListener("click", startGame); //adds event listener to start button to start the timer
 //startBtn.addEventListener("click", startGame); //adds event listener to start button to start the game
 
 function startTimer(event) {
-    event.preventDefault();//prevents timer from starting automatically
+    //event.preventDefault();//prevents timer from starting automatically
     // Sets interval in variable
     var timerInterval = setInterval(function() {
-        secondsLeft--;
-        timeEl.textContent = secondsLeft;
+      secondsLeft--;
+      timeEl.textContent = secondsLeft;
     
-        if(secondsLeft === 0) {
-        // Stops execution of action at set interval
+      if(secondsLeft === 0) {
+      // Stops execution of action at set interval
         timeEl.textContent = '';
         clearInterval(timerInterval);
-        // Use `clearInterval()` to stop the timer
-        endGame()
-        // Call the `displayMessage()` function
-        // displayMessage();
-        
-        }
+        endGame();
+        stopTimer();
+      }
     } ,1000);
-    startGame();
+    //startGame();
 
 }
-//---- Try Again Later
-//var wrongTime = document.querySelector("#wrong-opt");
 
-//wrongTime.addEventListener("click", subTime);
-//function setTime(event) {
-//    event.preventDefault();
-//}
-//------
-
-//timer section ends here
-
-
+function stopTimer() {
+  timeEl.textContent = '';
+  clearInterval(timerInterval);
+}
 
 //2. The quiz options
 var quizArea = document.getElementById("quiz");
@@ -53,14 +36,15 @@ var questionContent = document.getElementById("questionContainer");
 var questionText = document.getElementById("question");
 var questionsBtn = document.getElementById("answer-next-buttons");
 var quizTitlePage = document.getElementById("startPage");
+var answerText = document.querySelectorAll(".answer")
 
-//var questionElement = document.getElementById("question");
 var answerButtonsElement = document.getElementById("answer-next-buttons");
 var score; //this will be the seconds left
 
 var shuffledQuestions, currentQuestionIndex;
 
-var questionsBtn = [
+
+var questionsArray = [
   { // [0]
     question: "Commonly used data types do NOT include: ",
     answerChoices: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
@@ -90,140 +74,125 @@ var questionsBtn = [
 
 
 function startGame() {
-    console.log("started");
+  console.log("started");
 
-    startBtn.classList.add("hide");
-    quizTitlePage.classList.add("hide");
-    questionContent.classList.remove("hide");
-    questionText.classList.remove("hide");
-    shuffledQuestions = questionsBtn.sort(() => Math.random() -.5);
-    currentQuestionIndex = 0;
-    
-    showQuestion();
+  startBtn.classList.add("hide");
+  quizTitlePage.classList.add("hide");
+  questionContent.classList.remove("hide");
+  questionText.classList.remove("hide");
+  questionsArray = questionsArray.sort(() => Math.random() -.5);
+  currentQuestionIndex = 0;
 
-    //for (var i=0; i<4; i++){
-    //document.createElement("button").textContent = questionsBtn[currentQuestion].answerChoices[i]
-    //}
-  // start the quiz!
-  // 1. Start timer *done
-  // 2. create a question *done
-  // 3. create answer choices *done
-  // 4. add event listeners to my answer choice buttons and that will validate whether they chose the right answer or not
-  // 5. move on to next question
-
-  // this function will start your timer
-    //startTimer();
-  // this function will kick off rendering the question and answers to the page
-    //generateQuestion();
+  showQuestion();
+  generateAnswerChoices();
+  startTimer();
 }
 
 function generateNextQuestion() {
-    showQuestion(shuffledQuestions[currentQuestionIndex])
-    //question = questions[currentQuestion];
-  // create an element (p, div)
-    //var questionEl = document.createElement("p");
-  // write into that element using our question variable (textContent)
-    //questionEl.textContent(question);
-  // append that question element into our quiz area (appendChild)
-   // questionEl.appendChild(quizArea);
-  // generateAnswerChoices
-    //generateAnswerChoices();
+  validateAnswer(this.textContent);//
+  currentQuestionIndex++;
+  showQuestion();
+  generateAnswerChoices();
+  if (questionsArray.length <= currentQuestionIndex + 1) {
+    endGame();
+    stopTimer();
+  }
 }
 
 function showQuestion(){
-    questionText.innerText = questionsBtn.question;
-}
+  questionText.innerText = questionsArray[currentQuestionIndex].question;
 
-//imageContainer.addEventListener("click", function(event) {
-//   var element = event.target;
+  //if (questionsArray[lastIndexOf].question) {
+  //  questionContent.classList.add("hide");
+  //  questionText.classList.add("hide");
+  //  correctText.classList.add("hide");
+  //  wrongText.classList.add("hide");
+  //  endGamePage.classList.remove("hide");
+  //}
+}
 
 function generateAnswerChoices() {
   // for loop i < questions[currentQuestion].answerChoices.length
   // create an element (button)
-    var buttonEl = document.createElement("button");
-  // write into that element using our answerChoices variable (textContent)
-    buttonEl.textContent(answerChoices);
-  // add event listener btn.addEventListener("click", validateAnswer)
-    buttonEl.addEventListener("click", validateAnswer); 
-  // append that question element into our quiz area (appendChild)
-    buttonEl.appendChild(quizArea);
+  console.log(questionsArray[currentQuestionIndex]);
+  for (i=0; i<answerText.length; i++){
+    answerText[i].textContent = questionsArray[currentQuestionIndex].answerChoices[i];
+    answerText[i].addEventListener("click", generateNextQuestion);
+  }
+}
+var correctText = document.getElementById("correctTextDisplay");
+var wrongText = document.getElementById("wrongTextDisplay");
+
+function validateAnswer(buttonText) { 
+  
+  if (buttonText===questionsArray[currentQuestionIndex].correctAnswer){
+    correctText.classList.remove("hide");
+    wrongText.classList.add("hide");
+  }
+  else {
+    wrongText.classList.remove("hide");
+    correctText.classList.add("hide");
+    secondsLeft = secondsLeft -10; // subtracts ten seconds from timer when question is answered incorrectly
+  }
 }
 
-function validateAnswer(event) { //select answer fuction
-    //event.preventDefault();
-  // grab text of button that was clicked (event.target.textContent)
-    //var userChoice = 
-  // conditional statement test userChoice === correctAnswer
-  // true
-  //    correct answer code
-  // false
-  //    incorrect answer
-  //    decrease timer by 10 secs
-
-  // move onto the next question
-
-  // currentQuestion++
-  // conditional statement to check if you've reached the end of the questions array if(currentQuestion === questions.length)
-  // end the game (call endGame())
-    //endGame()
-  // reset quiz area (quizArea.innerHTML = "", loop using .removeChild())
-
-  // call generateQuestion again to start on the next question
-   // generateNextQuestion();
-}
-
-//function startTimer() {
-//  timer = setInterval(function() {
-//    if (timeLeft > 1) {
-//      // Set the `textContent` of `timerEl` to show the remaining seconds
- //     timerEl.textContent = timeLeft + ' seconds remaining';
-//      // Decrement `timeLeft` by 1
-//      timeLeft--;
-//    } else if (timeLeft === 1) {
-//      // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
-//      timerEl.textContent = timeLeft + ' second remaining';
-//      timeLeft--;
-//    }
-//  } ,1000)
-//}
 var endGamePage = document.getElementById("end-game");
-
+var finalScoreText = document.getElementById("finalScore");
 function endGame() {
     
   // end game whether it reaches the end of the quiz or time runs out
   // display none quiz area and display end game div
-    if (secondsLeft === 0){
-        alert('sorry, out of time');
-        questionContent.classList.remove("hide");
-        questionContent.classList.add("hide");
-        questionText.classList.add("hide");
-    }
-  // score
-    //score = timerCount;
-  // display score
-
-  // display high score
+  if (secondsLeft == 0){
+    questionContent.classList.add("hide");
+    questionText.classList.add("hide");
+    correctText.classList.add("hide");
+    wrongText.classList.add("hide");
+    endGamePage.classList.remove("hide");
+    //stopTimer();
+  }
+  //if (questionsArray[currentQuestionIndex].){}
+  if (questionsArray.length) {
+    questionContent.classList.add("hide");
+    questionText.classList.add("hide");
+    correctText.classList.add("hide");
+    wrongText.classList.add("hide");
+    endGamePage.classList.remove("hide");
+  }
+  
+  finalScoreText.textContent = secondsLeft;
 }
 
-//function saveScore(event) {
-    //event.preventDefault();
+var initialEl = document.getElementById("initialInput");
+var saveBtn = document.getElementById("saveScoreBtn")
+saveBtn.addEventListener("click", saveScore);
 
-    //var scoreObj = {
-    //intials: event.target.children[0].value,
-    //score: timerCount
-    //}
+function saveScore(event) {
+  event.preventDefault();
+  
+  var scoreObj = {
+    intials: initialEl.value,
+    score: secondsLeft,
+  }
+  console.log(scoreObj);
   // sets the score into local storage
-    //localStorage.setItem("score", JSON.stringify(scoreObj));
-//}
+  localStorage.setItem("score", JSON.stringify(scoreObj));
 
-//function getScore() {
+  getScore();
+}
+
+var highScoreList = document.getElementById("highScoreList");
+
+function getScore() {
+
   // get high score out of localstorage
-    //var score = JSON.parse(localStorage.getItem("score"))
+  var score = JSON.parse(localStorage.getItem("score"));
   // display to end game div
+  console.log(score);
+  var highScoreItemEl = document.createElement('li');
 
+  highScoreItemEl.textContent = score;
+
+  highScoreList.appendChild(highScoreItemEl);
+  
+}
 //}
-
-
-//scoreForm.addEventListener("submit", saveScore)
-
-
